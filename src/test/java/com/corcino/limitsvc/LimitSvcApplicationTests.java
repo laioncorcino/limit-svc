e9ed1eafef45c35944b93664e9303ac61d2d3d1c
Laion Corcino <laioncorcino@gmail.com>
2022-03-17 23:07:34 -0300

    feat: Initial commit

---
+++ src/test/java/com/corcino/limitsvc/LimitSvcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.corcino.limitsvc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LimitSvcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
